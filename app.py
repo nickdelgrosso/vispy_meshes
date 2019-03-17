eba6262559c5d9a45fa9c70cbb36655619ed6b3c
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-03-17 19:26:47 +0100

    added Camera module.

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-import numpy as np
 from vispy import app, gloo
-from vispy.util import transforms as tr
 from mesh import Mesh
-from shaders import current_shader
-np.set_printoptions(suppress=True, precision=2)
+from camera import Camera
 
 
 # Import Mesh
 monkey = Mesh('monkey.obj', position=[0, 0, -3], rotation=[0, 0, 0], scale=[1, 1, 1])
 
-# Send projection matrix to the shader
-projection_matrix = tr.perspective(90, 1.33, .1, 20)
-current_shader['projection_matrix'] = projection_matrix
-current_shader['view_matrix'] = tr.translate([0, 0, 0])
+camera = Camera()
 
 # Make Window
 canvas = app.Canvas(keys='interactive')
 
 @canvas.connect
 def on_draw(event):
+    camera.activate()
+
     gloo.clear([0, 0, 1])
     monkey.rotation[1] += .1
 
