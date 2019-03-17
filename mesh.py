5b63fa9b44de769f53819ca7a87e7fd800599905
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-03-17 19:55:34 +0100

    merged code into Physical class

--- mesh.py
+++ mesh.py
@@ -2,49 +2,21 @@ import numpy as np
 from vispy import io, gloo
 from vispy.util import transforms as tr
 from shaders import current_shader
+from physical import Physical
 
-
-class Mesh:
+class Mesh(Physical):
 
     def __init__(self, obj_filename, position=(0., 0., 0.), rotation=(0., 0., 0.), scale=(1., 1., 1.), color=(1., 0., 0.)):
+        Physical.__init__(self, position, rotation, scale)
         vertices, faces, normals, texcoords = io.read_mesh(obj_filename)
         assert len(vertices[0]) == 3, "Vertices are 3D!"
         assert len(faces[0]) == 3, "Mesh must be triangulated!"
         self.vertices = vertices - np.mean(vertices, axis=0)
         self.normals = normals
         self.faces = faces
-        self.position = list(position)
-        self.rotation = list(rotation)
-        self.scale = list(scale)
         self.color = list(color)
 
         self.load()
-
-    @property
-    def model_matrix(self):
-        """
-        Returns a 4x4 model matrix.
-
-        Arguments:
-            -translation: x, y, z coordintats
-            -rotation: x, y, z rotations (degrees)
-            -scale: x, y, z scale.
-
-        Returns:
-            -model_matrix: 4x4 array
-        """
-        sm = tr.scale(self.scale).T
-        rx, ry, rz = self.rotation
-        rzm = tr.rotate(rz, [0, 0, 1]).T
-        rym = tr.rotate(ry, [0, 1, 0]).T
-        rxm = tr.rotate(rx, [1, 0, 0]).T
-        trm = tr.translate(self.position).T
-        mm = trm @ rxm @ rym @ rzm @ sm
-        return mm
-
-    @property
-    def normal_matrix(self):
-        return np.linalg.inv(self.model_matrix.T)
 
     @property
     def vertex_buffer(self):
